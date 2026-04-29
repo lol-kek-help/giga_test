4f3a228105c6b2ba8bf20e2d8ad1462483ac4528
Даша Чугунова <72258767+lol-kek-help@users.noreply.github.com>
2026-04-29 21:49:43 +0300

    Fix getTaskByID JSON serialization

--- src/main/java/com/example/giga_test/TaskController.java
+++ src/main/java/com/example/giga_test/TaskController.java
@@ -1,6 +1,5 @@
 package com.example.giga_test;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
