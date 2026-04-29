38cb0fe9885ca713e1d4df762ca22dbd95224355
Даша Чугунова <72258767+lol-kek-help@users.noreply.github.com>
2026-04-30 01:06:41 +0300

    map и getallbyid

--- src/main/java/com/example/giga_test/TaskController.java
+++ src/main/java/com/example/giga_test/TaskController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TaskController {
     private final TaskService taskService;
@@ -16,6 +18,13 @@ public class TaskController {
     public Task getTaskByID(
             @PathVariable("id") int id
     ) {
+        System.out.println("get id");
         return taskService.getTaskByID(id);
+    }
+
+    @GetMapping("/")
+    public List<Task> getAllTask() {
+        System.out.println("get all");
+        return taskService.findAllTask();
     }
 }
--- src/main/java/com/example/giga_test/TaskService.java
+++ src/main/java/com/example/giga_test/TaskService.java
@@ -3,24 +3,60 @@ package com.example.giga_test;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 public class TaskService {
+    private final Map<Integer, Task> taskMap = Map.of(
+            1, new Task(
+                    1,
+                    "Test",
+                    "testtest",
+                    Status.NEW,
+                    Priority.MEDIUM,
+                    Category.GENERAL,
+                    new User(1, "Demo User"),
+                    new Operator(100, "Demo Operator"),
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusHours(8)
+            ),
+            2, new Task(
+                    2,
+                    "Test",
+                    "testtest",
+                    Status.NEW,
+                    Priority.MEDIUM,
+                    Category.GENERAL,
+                    new User(1, "Demo User"),
+                    new Operator(100, "Demo Operator"),
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusHours(8)
+            ),
+            3, new Task(
+                    3,
+                    "Test",
+                    "testtest",
+                    Status.NEW,
+                    Priority.MEDIUM,
+                    Category.GENERAL,
+                    new User(1, "Demo User"),
+                    new Operator(100, "Demo Operator"),
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusHours(8))
+    );
+
+    public List<Task> findAllTask() {
+        return taskMap.values().stream().toList();
+    }
+
     public Task getTaskByID(
             int id
     ) {
-        return new Task(
-                id,
-                "Test",
-                "testtest",
-                Status.NEW,
-                Priority.MEDIUM,
-                Category.GENERAL,
-                new User(1, "Demo User"),
-                new Operator(100, "Demo Operator"),
-                LocalDateTime.now(),
-                LocalDateTime.now().plusHours(8)
-
-        );
+        if (!taskMap.containsKey(id)){
+            throw new NoSuchElementException("No that id = " + id);
+        }
+        return taskMap.get(id);
     }
 }
